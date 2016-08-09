105ccec7fd25d255155de8b327be4b322716d826
Celso Mireles <celso.mireles@gmail.com>
2016-08-08 21:00:11 -0700

    Create a store with SMS messages

--- dev/js/index.js
+++ dev/js/index.js
@@ -6,12 +6,14 @@ import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
 import createLogger from 'redux-logger';
+import allReducers from './reducers';
 import App from './components/App.js';
 
 
 const logger = createLogger();
 const store = createStore(
-    applyMiddleware(thunk, promise, logger)
+  allReducers,
+  applyMiddleware(thunk, promise, logger)
 );
 
 ReactDOM.render(
---
+++ dev/js/reducers/reducer-list.js
@@ -0,0 +1,16 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            message: 'Check out the Cosecha Wiki: http://wiki.movimientocosecha.com'
+        },
+        {
+            id: 2,
+            message: 'Check out Cosecha on Facebook: http://facebook.com/MovimientoCosecha'
+        },
+        {
+            id: 3,
+            message: 'Check out the Cosecha Website: http://movimientocosecha.com'
+        }
+    ]
+}
