9f7621047d3f716c3c9802e0fa8d711f1f4ff5b2
Celso Mireles <celso.mireles@gmail.com>
2016-08-13 13:03:56 -0700

    Test a webpack edit.

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,6 @@ module.exports = {
     plugins: debug ? [] : [
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: true})
     ]
 };
