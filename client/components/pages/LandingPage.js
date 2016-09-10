4ac8ef5dfa14485991cc13705c1739ad5f7765e7
Celso Mireles <celso.mireles@gmail.com>
2016-09-09 18:32:43 -0700

    Add messages

--- client/components/pages/LandingPage.js
+++ client/components/pages/LandingPage.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import { Layout, Panel, Button, Input, Checkbox } from 'react-toolbox';
+import { Layout, Panel, Button, Input, RadioGroup, RadioButton } from 'react-toolbox';
 import theme from './LandingPage.scss';
-import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 const style = {
 	bg: {
 		'background': 'rgba(250, 250, 250, 0.8)',
-		'maxWidth': '400px',
+		'maxWidth': '300px',
 		'padding': '30px',
 		'margin': '0px auto'
 	}
@@ -24,17 +23,23 @@ class LandingPage extends Component {
         <Panel className="overlay" >
           <div style={style.bg}>
 	          <h1>Cosecha SMS</h1>
-	          <p>Select the message you want to send:</p>
+	          
+						<ol>
+							<li>Movimiento Cosecha: http://movimientocosecha.com</li>
+							<li>Dignidad y Respeto: https://www.facebook.com/movimientocosecha</li>
+							<li>Organiza Con Cosecha: http://wiki.movimientocosecha.com</li>
+						</ol>
 
 						<br />
-						
-						<Checkbox label="Message 1" />
-						<Checkbox label="Message 2" />
-						<Checkbox label="Message 3" />
+						<RadioGroup value='3' >
+							<RadioButton label="Message 1" value='1' />
+							<RadioButton label="Message 2" value='2' />
+							<RadioButton label="Message 3" value='3' />
+						</RadioGroup>
 
 						<Input label="Phone Number" name="phone" type="tel" />
 
-            <Link to={'/terms'} ><Button icon='add' label="Send SMS" raised primary /></Link>
+            <Button icon='add' label="Send SMS" raised primary />
           </div>
         </Panel>
       </Layout>
